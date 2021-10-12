a5f12ec33fda3bc1e3e3dfb457ce61d9b8d2fb49
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-12 20:07:22 -0300

    :sparkles: create sign in page

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const { signIn } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.content}>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={event => setEmail(event.target.value)}
+          placeholder="E-mail"
+        />
+
+        <input 
+          type="password" 
+          value={password}
+          onChange={event => setPassword(event.target.value)}  
+          placeholder="Password"
+        />
+
+        <button type="submit">
+          Sign in
+        </button>
+      </form>
+    </main>
+  )
+}
