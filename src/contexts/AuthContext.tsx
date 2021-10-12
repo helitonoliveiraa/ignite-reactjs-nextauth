47469ee418142e0e51403040d1ecd9977eae5aa8
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-12 20:32:57 -0300

    :wrench: recovery authentication state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import Router from 'next/router';
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 import { api } from "../services/api";
 
@@ -37,6 +37,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { '@nextauth:token': token } = parseCookies();
+
+    if (token) {
+      api.get('/me').then(response => {
+        const {  email, permissions, roles } = response.data as User;
+
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post<ResponseData>('sessions', {
@@ -60,6 +76,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push('/dashboard');
     } catch (err) {
