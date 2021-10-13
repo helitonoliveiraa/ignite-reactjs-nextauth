d9cdd7e642d3203ab484918daa5ee7d785003a11
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-12 21:27:47 -0300

    :wrench: request queue on Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
 
 type ResponseToken = {
   token: string;
@@ -23,25 +25,54 @@ api.interceptors.response.use(response => {
       cookies = parseCookies();
 
       const { '@nextauth:refreshToken': refreshToken } = cookies;
+      const originalConfig = error.config;
 
-      api.post('/refresh', {
-        refreshToken,
-      }).then(response => {
-        const { token, refreshToken: newRefreshToken } = response.data as ResponseToken;
+      if (!isRefreshing) {
+        isRefreshing = true;
 
-        setCookie(null, '@nextauth:token', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days    
-          path: '/',  
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token, refreshToken: newRefreshToken } = response.data as ResponseToken;
+  
+          setCookie(null, '@nextauth:token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days    
+            path: '/',  
+          });
+  
+          setCookie(null, '@nextauth:refreshToken', newRefreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days    
+            path: '/',  
+          });
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+          failedRequestQueue.forEach(request => 
+              request.onSuccess(token)
+            );
+          failedRequestQueue = [];
+        }).catch(err => {
+          failedRequestQueue.forEach(request => 
+            request.onSuccess(err)
+          );
+        failedRequestQueue = [];
+        }).finally(() => {
+          isRefreshing = false;
         });
-
-        setCookie(null, '@nextauth:refreshToken', newRefreshToken, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days    
-          path: '/',  
-        });
-
-        api.defaults.headers['Authorization'] = `Bearer ${token}`;
-      });
+      }
       
+      return new Promise((resolve, reject) => {
+        failedRequestQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+            resolve(api(originalConfig));
+          },
+          onFailed: (err: AxiosError) => {
+            reject(err);
+          },
+        });
+      });
     } else {
 
     }
