1b163b205bbb3d1a034446e51146b354f3a94340
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-17 17:24:19 -0300

    :sparkles: feat: validate access permissions on content page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -32,8 +32,6 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
 
   const response = await apiClient.get('/me');
 
-  console.log(response.data);
-
   return {
     props: {},
   }
--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,16 +1,45 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
-import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from 'jwt-decode';
 
-export function withSSRAuth<p>(fn: GetServerSideProps<p>): GetServerSideProps {
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<p>(fn: GetServerSideProps<p>, options?: WithSSRAuthOptions): GetServerSideProps {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<p>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['@nextauth:token'];
 
-    if (!cookies['@nextauth:token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false,
+        }
+      }
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[], roles: string[] }>(token);
+      const { permissions, roles } = options;
+  
+      const userHasValidatePermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidatePermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          }
         }
       }
     }
